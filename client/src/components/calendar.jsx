03b2a8529af1501a73485b65b6520e5aaf0aad41
essitormanen <essioliviatormanen@gmail.com>
2024-11-20 20:56:52 +0100

    added fetch to the client/calendar.jsx

--- client/src/components/calendar.jsx
+++ client/src/components/calendar.jsx
@@ -2,27 +2,60 @@ import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom';
 
 
-export default function calendar(){
-    let [events, setEvents] = useState([]);
+export default function Calendar(){
+    // let [events, setEvents] = useState([]);
 
-    useEffect(() => {
-        getEvents();
-    }, []); 
+    // useEffect(() => {
+    //     getEvents();
+    // }, []); 
+        
+    //     //the getEvents function fetches the event from the database
+    //     async function getEvents(){
+    //         fetch("/api/event")
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             setEvents(data);
+    //         })
+    //         .catch(e => {
+    //             console.log("Network Error", e);
+    //         });
+    //     };
     
-    //the getEvents function fetches the event from the database
-    const getEvents = () => {
-        fetch("/api/event")
-        .then(response => response.json())
-        .then(data => {
-            setEvents(data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    };
+
+        let[events, setEvents] = useState([]);
+        // let eventID = useParams().id
+        
+        useEffect(() => {
+          getEvents(); 
+        }, []); //[] is for calling the cb just once after the first render
+      
     
+        async function getEvents() {
+          
+          try{
+            console.log("calendar events")
+          
+            // console.log(eventID)
+            let response = await fetch(`/api/event`);
+            if(response.ok){
+              let data = await response.json();
+              setEvents(data)
+              console.log(data)
+            }
+            else{
+              console.log(`Server Error: ${response.status} ${response.statusText}`);
+      
+            }
+          }
+          catch(e){
+            console.log("Network Error", e);
+          }
+        }
+
     return (
         <div className = 'Calendar'>
+            <p> Voihan vittu nyt</p>
             <ul>
                 {
                 events.map (s =>(
